9959dba6062d83e43b7033d044da6f6f0bf1ceab
Vasil Shotarov <vshotarov@gmail.com>
2022-04-03 12:56:24 +0100

    Implemented profile mode home page of papermod

---
+++ tests/integration/custom_theme2/custom_theme2.py
@@ -0,0 +1,17 @@
+url = "https://simple_website.xyz"
+title = "Simple Website"
+description = "My simple website's description"
+version = "0.1"
+copyright_year = "2022"
+aggregate = ["portfolio","blog"]
+social_icons = [
+		{ "href": "/", "title": "GitHub", "img":"/icons/github.svg"},
+		{ "href": "/", "title": "Twitter", "img":"/icons/twitter.svg"}
+]
+home_page_buttons = [
+		{ "href" :"/", "title": "button 1" },
+		{ "href" :"/", "title": "button 2" },
+		{ "href" :"/", "title": "button 3" },
+		{ "href" :"/", "title": "button 4" }
+]
+dark_mode = False
