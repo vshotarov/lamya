e89a69c5364560bf266b015e45f5ae277571c7c1
Vasil Shotarov <vshotarov@gmail.com>
2022-05-10 09:34:34 +0100

    Add mobile menu toggle button

--- src/pygeon/themes/pygeon/static/js/nav_aria.js
+++ src/pygeon/themes/pygeon/static/js/nav_aria.js
@@ -37,3 +37,31 @@ Array.prototype.forEach.call(menuItems, function(el, i){
 		event.preventDefault();
 	});
 });
+
+// https://stackoverflow.com/questions/1248081/how-to-get-the-browser-viewport-dimensions
+var menu = document.getElementById("menu"); 
+var menu_toggle = document.getElementById("menu-toggle"); 
+if (document.documentElement.clientWidth <= 768) {
+	menu.setAttribute("aria-expanded", "false");
+	menu.parentNode.style.display = "none"; 
+}
+
+window.addEventListener("resize", function(event) {
+	if (document.documentElement.clientWidth <= 768) {
+		menu.setAttribute("aria-expanded", "false");
+		menu.parentNode.style.display = "none"; 
+	} else {
+		menu.setAttribute("aria-expanded", "true");
+		menu.parentNode.style.display = "block"; 
+	}
+}, true);
+
+menu_toggle.addEventListener("click", function(event) {
+	if (menu.parentNode.style.display == "none") {
+		menu.setAttribute("aria-expanded", "true");
+		menu.parentNode.style.display = "block"; 
+	} else {
+		menu.setAttribute("aria-expanded", "false");
+		menu.parentNode.style.display = "none"; 
+	}
+});
