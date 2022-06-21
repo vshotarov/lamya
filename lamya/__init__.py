29da072c583d2dc2acd2502f6b1f6091cd89deab
Vasil Shotarov <vshotarov@gmail.com>
2022-06-21 09:04:36 +0100

    Add docstring to __init__.py

--- lamya/__init__.py
+++ lamya/__init__.py
@@ -1,3 +1,7 @@
+"""The lamya (emphasis is on the ya - lamyà) package provides both an out of
+the box markdown static site generator and a framework for building your own
+custom ones.
+"""
 from .content_tree import ContentTree, Folder, Root, AbstractPageOrPost,\
     PageOrPost, ProceduralPage, AggregatedPage, PaginatedAggregatedPage,\
     AggregatedGroupsPage
